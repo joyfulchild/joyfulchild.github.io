9612fe1ec146a9c6c9a3532007b31afb383a5789
Andrew Gonzales <78248811+agonzales95@users.noreply.github.com>
2024-09-20 10:32:24 -0500

    Meta Data Updates
    Updated Meta for SEO

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 // Define webpage routes
 app.get('/', (req, res) => {
   res.render('index', {
-    pageTitle: 'Daycare & Preschool | The Joyful Child',
-    metaDescription: 'We provide an accredited play-based child care program in a home-like environment. Learn how we can bring joy to your family.',
+    pageTitle: 'The Joyful Child: Daycare & Preschool',
+    metaDescription: 'We provide an accredited play-based child care program in a home-like environment. Join our growing community of families and experience the joys of childhood.',
     metaKeywords: 'the joyful child, the joyful child daycare, del valle daycare, austin daycare, del valle child care, austin child care, play-based daycare, play-based child care, daycare near me, preschool in austin, preschool in del valle',
     // Open Graph
-    ogTitle: 'Daycare & Preschool - The Joyful Child',
-    ogDescription: 'We provide an accredited play-based child care program in a home-like environment. Learn how we can bring joy to your family.',
+    ogTitle: 'The Joyful Child: Daycare & Preschool',
+    ogDescription: 'We provide an accredited play-based child care program in a home-like environment. Join our growing community of families and experience the joys of childhood.',
     ogImage: '/public/src/images/page-asset/landing/looking-bugs.jpg',
     pageUrl: 'https://thejoyfulchild.care/',
   });
@@ -40,22 +40,22 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
   res.render('contact', {
     pageTitle: 'Contact | The Joyful Child',
-    metaDescription: 'Are you looking for childcare? Reach out to us and we can help enroll into our program.',
+    metaDescription: 'Are you looking for an affordable childcare program for your family? Reach out to us and we can help enroll into our program.',
     metaKeywords: 'the joyful child contact, the joyful child email, the joyful child phone number, child care tuition',
     // Open Graph
     ogTitle: 'Contact | The Joyful Child',
-    ogDescription: 'Are you looking for childcare? Reach out to us and we can help enroll into our program.',
+    ogDescription: 'Are you looking for an affordable childcare program for your family? Reach out to us and we can help enroll into our program.',
     ogImage: '/public/src/images/page-asset/contact/og-contact.jpg',
     pageUrl: 'https://thejoyfulchild.care/contact/',
   });
 });
 app.get('/programs', (req, res) => {
   res.render('programs', {
-    pageTitle: 'Play-based Programs | The Joyful Child',
+    pageTitle: 'Play-based Childcare Programs | The Joyful Child',
     metaDescription: 'Play-based child care allows the best growth for children. Learn how our enviornment allows our children to grow naturally into themselves.',
     metaKeywords: 'Infant care, Toddler care, Preschool care, play-based child care, play based child care, early childhood education',
     // Open Graph
-    ogTitle: 'Play-based Programs | The Joyful Child',
+    ogTitle: 'Play-based Childcare Programs | The Joyful Child',
     ogDescription: 'Play-based child care allows the best growth for children. Learn how our enviornment allows our children to grow naturally into themselves.',
     ogImage: '/public/src/images/page-asset/programs/all-children.jpg',
     pageUrl: 'https://thejoyfulchild.care/programs/',
@@ -63,24 +63,24 @@ app.get('/programs', (req, res) => {
 });
 app.get('/philosophy', (req, res) => {
   res.render('philosophy', {
-    pageTitle: 'Child Centered Philosophy | The Joyful Child',
-    metaDescription: 'We believe that your child will grow best simply by being at the center of their journey.',
+    pageTitle: 'Child Centered Teaching Philosophy | The Joyful Child',
+    metaDescription: 'We believe your child thrives by being at the center of their journey. Learn how we can help them become their best self.',
     metaKeywords: 'child care philosophy, the joyful child philosophy, the joyful child teaching philosophy',
     // Open Graph
-    ogTitle: 'Child Centered Philosophy | The Joyful Child',
-    ogDescription: 'We believe that your child will grow best simply by being at the center of their journey.',
+    ogTitle: 'Child Centered Teaching Philosophy | The Joyful Child',
+    ogDescription: 'We believe your child thrives by being at the center of their journey. Learn how we can help them become their best self.',
     ogImage: '/public/src/images/page-asset/philosophy/child-centered.jpg',
     pageUrl: 'https://thejoyfulchild.care/philosophy/',
   });
 });
 app.get('/locations', (req, res) => {
   res.render('locations', {
-    pageTitle: 'Del Valle and Austin Child Care | The Joyful Child',
-    metaDescription: 'Does your family live in East Austin or Del Valle and need child care? Check which of our locations is closest to your home.',
+    pageTitle: 'Childcare for East Austin and Del Valle | The Joyful Child',
+    metaDescription: 'We provide child care for East Austin and Del Valle families. Look at our map to see which of our locations is closest to your home.',
     metaKeywords: 'the joyful child locations, child care near me, daycare near me, austin child care, del valle child care, austin daycare, del valle daycare, East Austin, Del Valle',
     // Open Graph
-    ogTitle: 'Del Valle and Austin Child Care | The Joyful Child',
-    ogDescription: 'Does your family live in East Austin or Del Valle and need child care? Check which of our locations is closest to your home.',
+    ogTitle: 'Childcare for East Austin and Del Valle | The Joyful Child',
+    ogDescription: 'We provide child care for East Austin and Del Valle families. Look at our map to see which of our locations is closest to your home.',
     ogImage: '/public/src/images/page-asset/contact/hornsby-bend-location.jpg',
     pageUrl: 'https://thejoyfulchild.care/locations/',
   });
